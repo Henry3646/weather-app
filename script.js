158befeb803510ad1c95ef123ae2bbebea3f5c55
Henry Arlt <arlthenry54@gmail.com>
2022-07-19 14:10:17 -0500

    added search bar

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ const feelslike = document.getElementById('feelslike')
 const wind = document.getElementById('wind')
 const humidity = document.getElementById('humidity')
 const loca = document.getElementById('location')
-let city = 'minneapolis'
+const form = document.querySelector('form')
+const subbtn = document.querySelector('.submitform')
+form.addEventListener('submit', changeLocation)
+subbtn.addEventListener('click', changeLocation)
+
+function changeLocation(e) {
+    e.preventDefault()
+    changeCity()
+}
+
+function changeCity() {
+    const input = document.querySelector('input[type="text"]')
+    const city = input.value
+    getWeather(city)
+}
 
 async function getWeather(city) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=0a6d8399d56a937c512b420da7a2f9fb`, {mode: 'cors'})
@@ -12,9 +26,9 @@ async function getWeather(city) {
     console.log(weatherData)
 
     temp.textContent = Math.floor(KtoF(weatherData.main.temp))
-    feelslike.textContent = 'Feels like - ' + Math.floor(KtoF(weatherData.main.feels_like))
-    wind.textContent = 'Wind - ' + weatherData.wind.speed + ' mph'
-    humidity.textContent = 'Humidity - ' + weatherData.main.humidity + '%'
+    feelslike.textContent = 'FEELS LIKE - ' + Math.floor(KtoF(weatherData.main.feels_like))
+    wind.textContent = 'WIND - ' + weatherData.wind.speed + ' mph'
+    humidity.textContent = 'HUMIDITY - ' + weatherData.main.humidity + '%'
     loca.textContent = weatherData.name + ', ' + weatherData.sys.country
 }
 
